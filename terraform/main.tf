abde9b6254da4e0711c24573a16be407fb197d22
Your Name <you@example.com>
2024-03-05 23:12:52 +0100

    Modified the main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,29 @@ provider "aws" {
 resource "aws_instance" "bootcamp" {
   ami                    = "ami-047e03b8591f2d48a"
   instance_type          = "t2.micro"
-
+  vpc_security_group_ids = [aws_security_group.allow_80.id]
   tags = {
     Name = "bootcamp"
   }
 }
+
+resource "aws_security_group" "allow_80" { 
+  name                  = "allow_80" 
+  description           = "Allow 80 for external access" 
+  ingress { 
+  description           = "80 from outside" 
+    from_port           = 80 
+    to_port             = 80 
+    protocol            = "tcp" 
+    cidr_blocks         = ["0.0.0.0/0"] 
+  }
+  egress { 
+    from_port           = 0 
+    to_port             = 0 
+    protocol            = "-1"  
+    cidr_blocks         = ["0.0.0.0/0"]
+    } 
+    tags = { 
+      Name = "allow_80" 
+      } 
+    }  
